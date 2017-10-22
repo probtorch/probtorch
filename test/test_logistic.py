4241f9dd12783da4b23046025220c9d91bcc86a1
Jan-Willem van de Meent <j.vandemeent@northeastern.edu>
2017-10-22 12:10:40 -0400

    Updated logistic distribution
    - support new sample size syntax
    - do not upcast scalar parameters to Variables
    - add tests for broadcasting semantics

--- test/test_logistic.py
+++ test/test_logistic.py
@@ -1,3 +1,4 @@
+import math
 from scipy.stats import logistic, kstest
 import numpy as np
 from probtorch.distributions.logistic import Logistic
@@ -5,8 +6,8 @@ import torch
 from common import TestCase, run_tests, SAMPLE_COUNT
 from torch.autograd import Variable
 
-class TestLogistic(TestCase):
 
+class TestLogistic(TestCase):
     def test_logprob(self):
         mu = Variable(torch.randn(100))
         s = torch.exp(Variable(torch.randn(100)))
@@ -50,14 +51,40 @@ class TestLogistic(TestCase):
     def test_sample(self):
         # TODO: this only works for scalar continuous distributions,
         # just to make sure things are ok until we write a better sample test
-        mu = Variable(torch.randn(1))
-        s = torch.exp(Variable(torch.randn(1)))
+        mu = torch.randn(1)[0]
+        s = math.exp(torch.randn(1)[0])
         dist = Logistic(mu, s)
-        samples = torch.zeros(SAMPLE_COUNT)
-        for n in range(SAMPLE_COUNT):
-            samples[n] = dist.sample().data[0]
-        _, p = kstest(samples.numpy(), 'logistic', (mu.data.numpy(), s.data.numpy()))
+        samples = dist.sample(SAMPLE_COUNT).data
+        _, p = kstest(samples.numpy(), 'logistic', (mu, s))
         assert p > 0.05
+
+    def test_size(self):
+        for dims in range(1, 4):
+            sizes = range(1, 1 + dims)
+            # ensure that sample size is handled correctly
+            for k in range(dims):
+                mu = Variable(torch.randn(*sizes[k:]))
+                s = Variable(torch.exp(torch.randn(*sizes[k:])))
+                dist = Logistic(mu, s)
+                value = dist.sample(*sizes[:k])
+                self.assertEqual(sizes, value.size())
+            # ensure that log_prob broadcasts values if needed
+            mu = Variable(torch.randn(*sizes))
+            s = Variable(torch.exp(torch.randn(*sizes)))
+            for k in range(dims):
+                dist = Logistic(mu, s)
+                value = Variable(torch.randn(*sizes[k:]))
+                log_prob = dist.log_prob(value)
+                self.assertEqual(sizes, log_prob.size())
+            # ensure that log_prob broadcasts parameters if needed
+            value = Variable(torch.randn(*sizes))
+            for k in range(dims):
+                mu = Variable(torch.randn(*sizes[k:]))
+                s = Variable(torch.exp(torch.randn(*sizes[k:])))
+                dist = Logistic(mu, s)
+                log_prob = dist.log_prob(value)
+                self.assertEqual(sizes, log_prob.size())
+
 
 if __name__ == '__main__':
     run_tests()

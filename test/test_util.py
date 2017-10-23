94153e2e628ad22229c09042c6d44a90126e3827
Jan-Willem van de Meent <j.vandemeent@northeastern.edu>
2017-10-23 11:36:28 -0400

    remove util.log_softmax and util.softmax
    These functions were implemented to all a dimension argument to be
    passed. PyTorch master now implements this argument, which means
    that these functions are no longer needed.

--- test/test_util.py
+++ test/test_util.py
@@ -5,32 +5,8 @@ from probtorch import util
 
 zero = Variable(torch.Tensor([0.0]))
 
+
 class TestWrap2D(TestCase):
-    def test_softmax(self):
-        for dims in range(1, 5):
-            sizes = range(1, 1 + dims)
-            r = Variable(torch.rand(*sizes))
-            for dim in range(dims):
-                # ensure that values sum to 1 along specified dim
-                sr = util.softmax(r, dim)
-                srs = sr.sum(dim).data
-                self.assertEqual(srs, torch.ones(srs.size()))
-            # ensure that default normalizes along last dim
-            self.assertEqual(sr, util.softmax(r))
-
-    def test_log_softmax(self):
-        for dims in range(1, 5):
-            sizes = range(1, 1 + dims)
-            r = Variable(torch.rand(*sizes))
-            for dim in range(dims):
-                lsr = util.log_softmax(r, dim)
-                # ensure that values sum to 1 along specified dim
-                sr = torch.exp(lsr)
-                srs = sr.sum(dim).data
-                self.assertEqual(srs, torch.ones(srs.size()))
-            # ensure that default normalizes along last dim
-            self.assertEqual(lsr, util.log_softmax(r))
-
     def test_log_sum_exp(self):
         for dims in range(1, 5):
             sizes = range(1, 1 + dims)
@@ -44,9 +20,6 @@ class TestWrap2D(TestCase):
                 self.assertEqual(r.exp().sum(dim).log(),
                                  util.log_sum_exp(r, dim))
                 for keepdim in [True, False]:
-                    # print('dims=%d, dim=%d keepdim=%s' % (dims, dim, keepdim))
-                    # print(r.exp().sum(dim, keepdim).log())
-                    # print(util.log_sum_exp(r, dim, keepdim))
                     self.assertEqual(r.exp().sum(dim, keepdim).log(),
                                      util.log_sum_exp(r, dim, keepdim))
 
@@ -63,11 +36,9 @@ class TestWrap2D(TestCase):
                 self.assertEqual(r.exp().mean(dim).log(),
                                  util.log_mean_exp(r, dim))
                 for keepdim in [True, False]:
-                    # print('dims=%d, dim=%d keepdim=%s' % (dims, dim, keepdim))
-                    # print(r.exp().sum(dim, keepdim).log())
-                    # print(util.log_sum_exp(r, dim, keepdim))
                     self.assertEqual(r.exp().mean(dim, keepdim).log(),
                                      util.log_mean_exp(r, dim, keepdim))
+
 
 if __name__ == '__main__':
     run_tests()

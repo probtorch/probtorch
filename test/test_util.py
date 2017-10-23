436e8f813d04497dcb3844edaa923597e65f3f99
Siddharth N <siddharth@iffsid.com>
2017-10-24 00:51:09 +0100

    add test for partial_sum

--- test/test_util.py
+++ test/test_util.py
@@ -1,12 +1,27 @@
 import torch
+from random import randint, sample
 from torch.autograd import Variable
 from common import TestCase, run_tests
 from probtorch import util
 
 zero = Variable(torch.Tensor([0.0]))
 
-
 class TestWrap2D(TestCase):
+    def test_partial_sum(self):
+        for dims in range(1, 5):
+            size = [randint(1, 5) for _ in range(dims)]
+            r = Variable(torch.rand(*size))
+            # compute partial sum by iterative summation over keep_dims complement
+            keep_dims = sample(range(dims), randint(0, dims - 1))
+            res = r
+            if len(keep_dims) == 0:
+                res = res.sum()
+            else:
+                drop_dims = list(set(range(dims)) - set(keep_dims))
+                for d in sorted(drop_dims, reverse=True):
+                    res = res.sum(d)
+            self.assertEqual(res, util.partial_sum(r, keep_dims=keep_dims))
+
     def test_log_sum_exp(self):
         for dims in range(1, 5):
             sizes = range(1, 1 + dims)
@@ -38,7 +53,6 @@ class TestWrap2D(TestCase):
                 for keepdim in [True, False]:
                     self.assertEqual(r.exp().mean(dim, keepdim).log(),
                                      util.log_mean_exp(r, dim, keepdim))
-
 
 if __name__ == '__main__':
     run_tests()

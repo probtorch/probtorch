2b615fbf7c183e9b8128d3c6f9f01500b5666fbe
Jan-Willem van de Meent <j.vandemeent@northeastern.edu>
2017-10-23 11:17:59 -0400

    fixed error in probtorch.objectives for log_joint, log_sampled

--- probtorch/objectives/__init__.py
+++ probtorch/objectives/__init__.py
@@ -2,25 +2,27 @@ from . import montecarlo
 from . import importance
 from probtorch.util import batch_sum
 
+
 def log_joint(p, sample_dim=None, batch_dim=None):
-    log_P = None
+    log_P = 0.0
     for v in p:
         log_p = batch_sum(p[v].log_prob, sample_dim, batch_dim)
-        log_P = (log_P or 0.0) + log_p
+        log_P = log_P + log_p
     return log_P
 
 def log_observed(p, sample_dim=None, batch_dim=None):
-    log_P = None
+    log_P = 0.0
     for v in p:
         if p[v].observed:
             log_p = batch_sum(p[v].log_prob, sample_dim, batch_dim)
-            log_P = (log_P or 0.0) + log_p
+            log_P = log_P + log_p
     return log_P
 
+
 def log_sampled(p, sample_dim=None, batch_dim=None):
-    log_P = None
+    log_P = 0.0
     for v in p:
         if not p[v].observed:
             log_p = batch_sum(p[v].log_prob, sample_dim, batch_dim)
-            log_P = (log_P or 0.0) + log_p
+            log_P = log_P + log_p
     return log_P

db0a5ebedb9f8a44bfbda2a83b0fb8c1982479f7
Jan-Willem van de Meent <j.vandemeent@northeastern.edu>
2017-10-25 21:31:31 -0400

    refactored monte carlo objectives

--- probtorch/objectives/montecarlo.py
+++ probtorch/objectives/montecarlo.py
@@ -1,95 +1,160 @@
-from probtorch.util import batch_sum, log_sum_exp
-import torch
+from numbers import Number
+from torch.nn.functional import softmax
+from probtorch.util import sum_log_prob
 
-def log_like(q, p, sample_dim=None, batch_dim=None):
-    r"""Calculates a Monte Carlo estimate of the log-likelihood.
 
-    The expectation in this estimate is calculated with respect to
-    unobserved variables in the encoder trace.
+def elbo(q, p, sample_dim=None, batch_dim=None, alpha=0.1, beta=1.0):
+    r"""Calculates a Monte Carlo estimate of the semi-supervised evidence lower
+    bound (ELBO)
 
-    When `batch_dim` is specified, this function returns the mean
-    of the estimates for each item in the batch.
+    .. math::
+       E_{q(z | x, y)} \left[
+         \log p(x | y, z) - \beta \log \frac{q(z | x,y)}{p(z)}
+       \right]
+       + \alpha E_{q(z | x)}\left[ \log \frac{q(y, z| x)}{q(z | x)} \right]
 
-    When `sample_dim` is specified, this function computes an
-    importance sampling estimate for each item.
+    Where the sets of variables :math:`x`, :math:`y` and :math:`z` refer to:
+
+        :math:`x`: The set of conditioned nodes that are present in `p` but
+        are not present in `q`.
+
+        :math:`y`: The set of conditioned nodes in `q`.
+
+        :math:`z`: The set of sampled nodes in `q`.
+
+    Importance sampling is used to approximate the expectation over
+    :math:`q(z| x,y)`.
+
+    Arguments:
+        q(:obj:Trace): The encoder trace.
+        p(:obj:Trace): The decoder trace.
+        sample_dim(int, optional): The dimension containing individual samples.
+        batch_dim(int, optional): The dimension containing batch items.
+        alpha(float, default 0.1): Coefficient for the ML term.
+        beta(float, default 1.0):  Coefficient for the KL term.
     """
-    log_P = 0.0
-    log_W = None
-    for v in p:
-        if (v not in q):
-            log_p = batch_sum(p[v].log_prob,
-                              sample_dim=sample_dim,
-                              batch_dim=batch_dim)
-            log_P = log_P + log_p
+    log_weights = sum_log_prob(q, sample_dim, batch_dim,
+                               q.conditioned())
+    return (log_like(q, p, sample_dim, batch_dim, log_weights) -
+            beta * kl(q, p, sample_dim, batch_dim, log_weights) +
+            alpha * ml(q, sample_dim, batch_dim))
+
+
+def log_like(q, p, sample_dim=None, batch_dim=None, log_weights=None):
+    r"""Computes a Monte Carlo estimate of the log-likelihood.
+
+    .. math::
+       E_{q(z | x, y)}[\log p(x | y, z)]
+       \simeq \frac{1}{S} \frac{1}{B} \sum_{s=1}^S \sum_{b=1}^B
+              \log p(x^{(b)} | z^{(s,b)}, y^{(b)})
+
+    Where the sets of variables :math:`x`, :math:`y` and :math:`z` refer to:
+
+        :math:`x`: The set of conditioned nodes that are present in `p` but
+        are not present in `q`.
+
+        :math:`y`: The set of conditioned nodes in `q`.
+
+        :math:`z`: The set of sampled nodes in `q`.
+
+    Importance sampling is used to approximate the expectation over
+    :math:`q(z| x,y)`.
+
+    Arguments:
+        q(:obj:Trace): The encoder trace.
+        p(:obj:Trace): The decoder trace.
+        sample_dim(int, optional): The dimension containing individual samples.
+        batch_dim(int, optional): The dimension containing batch items.
+        log_weights(:obj:Variable or number, optional): Log weights for
+            samples. Calculated when not specified.
+    """
+    x = [n for n in p.conditioned() if n not in q]
+    log_prob = sum_log_prob(p, sample_dim, batch_dim, x)
     if sample_dim is None:
-        return log_P.mean()
+        return log_prob.mean()
     else:
-        for v in q:
-            if q[v].observed:
-                log_w = batch_sum(q[v].log_prob,
-                                  sample_dim=sample_dim,
-                                  batch_dim=batch_dim)
-                if log_W is None:
-                    log_W = 0.0
-                log_W = log_W + log_w
-            if log_W is None:
-                return log_P.mean(0).mean()
-            else:
-                W = torch.exp(log_W - log_sum_exp(log_W, 0, True))
-                return (W * log_P).sum(0).mean()
+        if log_weights is None:
+            log_weights = sum_log_prob(q, sample_dim, batch_dim,
+                                       q.conditioned())
+        if isinstance(log_weights, Number):
+            return log_prob.mean()
+        else:
+            weights = softmax(log_weights, 0)
+            return (weights * log_prob).sum(0).mean()
 
-def kl(q, p, alpha=0.0, sample_dim=None, batch_dim=None):
-    r"""Calculates a Monte Carlo estimate of the KL divergence.
 
-    The expectation in this estimate is calculated with respect to
-    unobserved variables in the encoder trace.
+def kl(q, p, sample_dim=None, batch_dim=None, log_weights=None):
+    r"""Computes a Monte Carlo estimate of the KL divergence.
 
-    When `batch_dim` is specified, this function returns the mean
-    of the estimates for each item in the batch.
+    .. math::
+       E_{q(z | x, y)}\left[ \log \frac{q(z | x,y)}{p(z)} \right]
+       \simeq \frac{1}{S} \frac{1}{B} \sum_{s=1}^S \sum_{b=1}^B
+       \left[ \log \frac{q(z^{(s,b)} | x^{(b)}, y^{(b)})}
+                        {p(z^{(s,b)})} \right]
 
-    When `sample_dim` is specified, this function computes an
-    importance sampling estimate for each item.
+    Where the sets of variables :math:`x`, :math:`y` and :math:`z` refer to:
+
+        :math:`x`: The set of conditioned nodes that are present in `p` but
+        are not present in `q`.
+
+        :math:`y`: The set of conditioned nodes in `q`.
+
+        :math:`z`: The set of sampled nodes in `q`.
+
+    Importance sampling is used to approximate the expectation over
+    :math:`q(z| x,y)`.
+
+    Arguments:
+        q(:obj:Trace): The encoder trace.
+        p(:obj:Trace): The decoder trace.
+        sample_dim(int, optional): The dimension containing individual samples.
+        batch_dim(int, optional): The dimension containing batch items.
+        log_weights(:obj:Variable or number, optional): Log weights for
+            samples. Calculated when not specified.
     """
-    log_P = 0.0
-    log_Q = 0.0
-    log_W = None
-    for v in q:
-        if (v in p):
-            log_p = batch_sum(p[v].log_prob,
-                              sample_dim=sample_dim,
-                              batch_dim=batch_dim)
-            log_q = batch_sum(q[v].log_prob,
-                              sample_dim=sample_dim,
-                              batch_dim=batch_dim)
-            log_P = log_P + log_p
-            log_Q = log_Q + log_q
-        if q[v].observed:
-            if log_W is None:
-                log_W = 0.0
-            log_W = log_W + log_q
+    z = list(q.sampled())
+    log_p = sum_log_prob(p, sample_dim, batch_dim, z)
+    log_q = sum_log_prob(q, sample_dim, batch_dim, z)
+    log_qp = (log_q - log_p)
     if sample_dim is None:
-        if log_W is None:
-            return (log_Q - log_P).mean()
-        else:
-            return (log_Q - log_P - (1.0 + alpha) * log_W).mean()
+        return log_qp.mean()
     else:
-        if log_W is None:
-            return (log_Q - log_P).mean(0).mean()
+        if log_weights is None:
+            log_weights = sum_log_prob(q, sample_dim, batch_dim,
+                                       q.conditioned())
+        if isinstance(log_weights, Number):
+            return log_qp.mean()
         else:
-            W = torch.exp(log_W - log_sum_exp(log_W, 0, True))
-            return (W * (log_Q - log_P)).sum(0).mean() - (1.0 + alpha) * log_W.mean(0).mean()
+            weights = softmax(log_weights, 0)
+            return (weights * log_qp).sum(0).mean()
 
-def elbo(q, p, alpha=0.0, sample_dim=None, batch_dim=None):
-    r"""Calculates a Monte Carlo estimate of the evidence lower bound.
 
-    The expectation in this estimate is calculated with respect to
-    unobserved variables in the encoder trace.
+def ml(q, sample_dim=None, batch_dim=None):
+    r"""Computes a Monte Carlo estimate of maximum likelihood encoder objective
 
-    When `batch_dim` is specified, this function returns the mean
-    of the estimates for each item in the batch.
+    .. math::
+       E_{q(z | x)}\left[ \log \frac{q(y, z| x)}{q(z | x)} \right]
+       \simeq \frac{1}{S} \frac{1}{B} \sum_{s=1}^S \sum_{b=1}^B
+       \left[ \log \frac{q( y^{(b)}, z^{(s,b)} | x^{(b)})}
+                        {q(z^{(s,b)} | x^{(b)})} \right]
 
-    When `sample_dim` is specified, this function computes an
-    importance sampling estimate for each item.
+    Where the sets of variables :math:`x`, :math:`y` and :math:`z` refer to:
+
+        :math:`x`: The set of conditioned nodes that are present in `p` but
+        are not present in `q`.
+
+        :math:`y`: The set of conditioned nodes in `q`.
+
+        :math:`z`: The set of sampled nodes in `q`.
+
+    Arguments:
+        q(:obj:Trace): The encoder trace.
+        p(:obj:Trace): The decoder trace.
+        sample_dim(int, optional): The dimension containing individual samples.
+        batch_dim(int, optional): The dimension containing batch items.
     """
-    return log_like(q, p, sample_dim, batch_dim) - \
-        kl(q, p, alpha, sample_dim, batch_dim)
+    log_q = sum_log_prob(q, sample_dim, batch_dim, q.conditioned())
+    if isinstance(log_q, Number):
+        return log_q
+    else:
+        return log_q.mean()

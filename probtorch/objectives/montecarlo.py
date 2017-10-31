ccb5507468ac16762e43310750df8890aa624b25
Siddharth N <siddharth@iffsid.com>
2017-10-31 00:36:25 +0000

    fixup reference in docstring

--- probtorch/objectives/montecarlo.py
+++ probtorch/objectives/montecarlo.py
@@ -145,8 +145,8 @@ def kl(q, p, sample_dim=None, batch_dim=None, log_weights=None,
            are returned without averaging or summation.
 
     References:
-        [1] Siddharth Narayanaswamy, Brooks Paige, Jan-Willem van de Meent,
-        Alban Desmaison, Frank Wood, Noah D Goodman, Pushmeet Kohli, and
+        [1] N. Siddharth, Brooks Paige, Jan-Willem van de Meent,
+        Alban Desmaison, Frank Wood, Noah D. Goodman, Pushmeet Kohli, and
         Philip HS Torr, Semi-Supervised Learning of Disentangled
         Representations, NIPS 2017.
     """

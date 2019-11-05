4442acbc06041b655d57988be5f119bb34669f2d
Alican Bozkurt <alican@ece.neu.edu>
2019-11-05 13:04:51 -0500

    fix deprecation warning in MutableMapping import

--- probtorch/stochastic.py
+++ probtorch/stochastic.py
@@ -1,4 +1,5 @@
-from collections import OrderedDict, MutableMapping
+from collections import OrderedDict
+from collections.abc import MutableMapping
 from .util import batch_sum, partial_sum, log_mean_exp
 import abc
 from enum import Enum

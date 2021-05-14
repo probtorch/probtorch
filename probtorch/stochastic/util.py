d50d095cfd725b5c39d8357681d6efe573bf7ba6
Sam Stites <stites@users.noreply.github.com>
2021-05-14 17:05:53 -0400

    add rerun with detached values to utils

--- probtorch/stochastic/util.py
+++ probtorch/stochastic/util.py
@@ -1,11 +1,13 @@
 from enum import Enum, auto, unique
-from probtorch.stochastic import Trace, Provenance
+from probtorch.stochastic import Trace, RandomVariable, ImproperRandomVariable
+
 
 @unique
 class WriteMode(Enum):
     LastWriteWins = auto()
     FirstWriteWins = auto()
     NoOverlaps = auto()
+
 
 def copytraces(*traces, exclude_nodes=None, mode=WriteMode.NoOverlaps):
     """
@@ -30,4 +32,35 @@ def copytraces(*traces, exclude_nodes=None, mode=WriteMode.NoOverlaps):
                     raise TypeError("impossible specification")
 
             newtrace._inject(rv, name=k, silent=True)
+    return newtrace
+
+
+def rerun_with_detached_values(trace: Trace):
+    """
+    Rerun a trace with detached values, recomputing the computation graph so that
+    value do not cause a gradient leak.
+    """
+    newtrace = Trace()
+
+    def rerun_rv(rv):
+        value = rv.value.detach()
+        if isinstance(rv, RandomVariable):
+            return RandomVariable(
+                value=value,
+                dist=rv.dist,
+                provenance=rv.provenance,
+                reparameterized=rv.reparameterized,
+            )
+        elif isinstance(rv, ImproperRandomVariable):
+            return ImproperRandomVariable(
+                value=value, log_density_fn=rv.log_density_fn, provenance=rv.provenance
+            )
+        else:
+            raise NotImplementedError(
+                "Only supports RandomVariable and ImproperRandomVariable"
+            )
+
+    for k, v in trace.items():
+        newtrace._inject(rerun_rv(v), name=k)
+
     return newtrace
